5a595efc4bae4146a891b343ef59753ca50440c3
thulanimbatha <thulanimbatha06@gmail.com>
2022-05-19 23:30:00 +0200

    done part 2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # use turtle to show screen
+from os import stat
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -14,17 +16,35 @@ turtle.shape(image)
 
 # turtle.onscreenclick(get_mouse_click_coor)
 
-answer_state = screen.textinput(title="Guess The State", prompt="What's another State's name?")
-print(answer_state)
+data = pandas.read_csv("50_states.csv") # access data
+all_states = data["state"].to_list()    # list of states
 
-turtle.mainloop()
+guessed_states = []
+
+while len(guessed_states) < 50:
+
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", 
+                                    prompt="What's another State's name?").title()
+    print(answer_state)
+
+    if answer_state == "Exit":
+        break
+    # check answer
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        # create turtle
+        state_name = turtle.Turtle()
+        state_name.hideturtle()
+        state_name.penup()
+        # access row data
+        state_data_row = data[data.state == answer_state]
+        state_name.goto(int(state_data_row["x"]), int(state_data_row["y"]))
+        state_name.write(answer_state)
+
+# turtle.mainloop()
 
 # screen.exit on click exits as soon as coordinate is found
 # screen.exitonclick() 
 
-# TODO 1: convert guess to title case
-# TODO 2: CHeck is the guess is among the 50 States
-# TODO 3: Write correct guess onto the map
-# TODO 4: Use a loop to allow the user to keep guessing
-# TODO 5: Record the correct guesses in a list
-# TODO 6: Keep track of the score
+# TODO 1: generate csv file containing incorrect/forgotten guessees
+
